6cf578ab2f309f02c52d6d60b85131060e200acd
MajidALILOUCH <majidach2001@gmail.com>
2023-04-11 02:35:44 +0000

    adding project

---
+++ 024 - Hidden Search Widget/main.js
@@ -0,0 +1,8 @@
+let search = document.querySelector(".search");
+let btn = document.querySelector(".btn");
+let input = document.querySelector(".input");
+
+btn.addEventListener("click", function () {
+  search.classList.toggle("active");
+  input.focus();
+});
---
+++ 025 - Blurry Loading/main.js
@@ -0,0 +1,26 @@
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let load = 0;
+
+let int = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+
+  if (load > 99) {
+    clearInterval(int);
+  }
+
+  loadText.innerText = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+// this function below help us to change the opacity from 1 to zero in the same amount of time that the load goes
+// from 0 to 100
+
+// https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+};
